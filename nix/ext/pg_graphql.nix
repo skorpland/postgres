6c81754c9fa33c888c41b5b6add99ef042fa08a0
samrose <samuel@supabase.io>
2025-03-24 22:03:48 -0400

    chore: set pg_graphql to stable rust (#1476)
    * chore: set pg_graphql to stable rust
    
    * chore: handle ports per version in test
    
    * chore: pushed too soon. rm typo
    
    * chore: suffix for testing
    
    * chore: bump
    
    * chore: remove unnecessary build flag
    
    * chore: bump
    
    * chore: need to link wal-g v2 correctly
    
    * chore: bump
    
    * Chore: bump to release

--- nix/ext/pg_graphql.nix
+++ nix/ext/pg_graphql.nix
@@ -1,8 +1,8 @@
 { lib, stdenv, fetchFromGitHub, postgresql, buildPgrxExtension_0_12_9, cargo, rust-bin }:
 
 let
-    rustVersion = "nightly";
-    cargo = rust-bin.nightly.latest.default;
+    rustVersion = "1.81.0";
+    cargo = rust-bin.stable.${rustVersion}.default;
 in
 buildPgrxExtension_0_12_9 rec {
   pname = "pg_graphql";
@@ -27,12 +27,11 @@ buildPgrxExtension_0_12_9 rec {
   # Setting RUSTFLAGS in env to ensure it's available for all phases
   env = lib.optionalAttrs stdenv.isDarwin {
     POSTGRES_LIB = "${postgresql}/lib";
-    PGPORT = "5434";
+    PGPORT = if (lib.versions.major postgresql.version) == "17" then "5440" else "5439";
     RUSTFLAGS = "-C link-arg=-undefined -C link-arg=dynamic_lookup";
     NIX_BUILD_CORES = "4";  # Limit parallel jobs
     CARGO_BUILD_JOBS = "4"; # Limit cargo parallelism
   };
-CARGO_BUILD_RUSTFLAGS = "--cfg tokio_unstable -C debuginfo=0";
   CARGO_PROFILE_RELEASE_BUILD_OVERRIDE_DEBUG = true;
 
   
