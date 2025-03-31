fae1fdfe0c9a6113578d887c447d6c89850d5b50
samrose <samuel@supabase.io>
2025-03-31 16:09:45 -0400

    chore: introduce tooling for pg 17.4 non-orioledb (#1420)
    * chore: introduce tooling for pg 17.4 non-orioledb rebase develop
    
    * chore: using md5 on order of query to make it work on all arch
    
    * chore: try to have migrations check just use start-server directly
    
    * chore: update regress tests
    
    * fix: install PG major version appropriate flake during upgrades
    
    * fix: exclude timescaledb for PG17+ upgrades
    
    * chore advance to pg 17.4
    
    * chore: remove pgjwt except for in pg 15.x
    
    * chore: roles already created
    
    * chore: try to resolve pgjwt for pg 15
    
    * chore: update regression tests
    
    * chore: update regression tests after rebase
    
    * chore: update schema for 17
    
    * chore: bring Dockerfile-17 up to date
    
    * chore: need to exclude migtest except for pg 15 on timescale plv8
    
    * chore: bump versions for testing
    
    * chore: advance versions for release
    
    * chore: suffix for Docker pre-release
    
    * chore: bump pre-release suffix
    
    * chore: bump for actual merge
    
    ---------
    
    Co-authored-by: Paul Cioanca <paul.cioanca@supabase.io>

--- migrations/db/init-scripts/00000000000000-initial-schema.sql
+++ migrations/db/init-scripts/00000000000000-initial-schema.sql
@@ -18,7 +18,15 @@ grant pg_read_all_data to supabase_read_only_user;
 create schema if not exists extensions;
 create extension if not exists "uuid-ossp"      with schema extensions;
 create extension if not exists pgcrypto         with schema extensions;
-create extension if not exists pgjwt            with schema extensions;
+do $$
+begin 
+    if exists (select 1 from pg_available_extensions where name = 'pgjwt') then
+        if not exists (select 1 from pg_extension where extname = 'pgjwt') then
+            create extension if not exists pgjwt with schema "extensions" cascade;
+        end if;
+    end if;
+end $$;
+
 
 -- Set up auth roles for the developer
 create role anon                nologin noinherit;
